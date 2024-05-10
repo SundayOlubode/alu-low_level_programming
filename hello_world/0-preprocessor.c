94eec209f0ed632db6037b5636ad281086f14d40
Sam <s.olubode@alustudent.com>
2024-05-10 20:52:56 +0100

    create: program files

--- hello_world/0-preprocessor.c
+++ hello_world/0-preprocessor.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Preprocessor
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("Hello World!")
-	return (0)
+	printf("Hello World!\n");
+	return (0);
 }
