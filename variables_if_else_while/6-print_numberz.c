9a6ab9966315ec7d2d8f6004ed708e9acb47ab6e
Sam <s.olubode@alustudent.com>
2024-05-17 09:20:49 +0100

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+	}
+
+	putchar('\n'); /* print new line */
+
+	return (0);
+}
