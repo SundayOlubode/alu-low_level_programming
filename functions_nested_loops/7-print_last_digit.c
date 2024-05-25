7ec0b5594c580a89a455c0e5b97fc4573d63a2f9
Sam <s.olubode@alustudent.com>
2024-05-25 10:09:40 +0100

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,10 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	_putchar(last_digit);
+	_putchar(last_digit + '0');
+
+	if(last_digit < 0)
+		last_digit *= -1;
 
 	return (last_digit);
 }
