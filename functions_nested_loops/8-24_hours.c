e12fe8837d83eb02bf15094c1e8742ecbe968327
Sam <s.olubode@alustudent.com>
2024-05-25 10:38:03 +0100

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,7 +14,9 @@ void jack_bauer(void)
 		min = 0;
 		while (min < 60)
 		{
-			_putchar((hour / 10) + '0');
+			int hr = hour / 10;
+
+			_putchar(hr + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
