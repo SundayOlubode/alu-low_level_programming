9d579aa63f3a4221cb43ffed2f6408e4f20e5356
Sam <s.olubode@alustudent.com>
2024-06-07 09:49:44 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Find string length
+ * @s: string pointer input
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
