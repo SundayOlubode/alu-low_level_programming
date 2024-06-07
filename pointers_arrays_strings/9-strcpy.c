ec962dd9cb1882f727c114df7c64f815d2e409bd
Sam <s.olubode@alustudent.com>
2024-06-07 12:47:12 +0100

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy: Copy String
+ * @dest: Destination
+ * @src: Source
+ * Return char *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
