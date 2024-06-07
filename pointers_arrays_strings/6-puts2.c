d5669f01aed8c74b0c161807b92b6d3b9766c180
Sam <s.olubode@alustudent.com>
2024-06-07 11:40:03 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,13 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0' && *(str - 1) != '\0' && *(str + 1) != '\0')
-	{
-		_putchar(*str);
-		str += 2;
-	}
+	int i;
+	int j = 0;
 
+	while (str[j] != '\0')
+		j++;
+
+	for (i = 0; i < j; i += 2)i
+		_putchar(str[i]);
 	_putchar('\n');
 }
