961b97ba813fd61886e13d035d03dc149764a552
Sam <s.olubode@alustudent.com>
2024-06-07 10:50:05 +0100

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (str[len] != '\0')
+	while (s[len] != '\0')
 	{
 		len++;
 	}
 
 	for (len--; len >= 0; len--)
 	{
-		_putchar(str[len]);
+		_putchar(s[len]);
 	}
 
 	_putchar('\n');
