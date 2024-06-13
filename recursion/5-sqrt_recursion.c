9324eb8e5d28f26cbfde4f2b213d6cd3c16d9791
Sam <s.olubode@alustudent.com>
2024-06-13 14:03:12 +0100

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+int root_helper(int n, int root);
+
+/**
+ * _sqrt_recursion - Find square root recursively
+ * @n: int arg
+ * Return: int
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (root_helper(n, 0));
+}
+
+/**
+ * root_helper - Helper function
+ * @n: int arg
+ * @root: int arg
+ * Return: int
+ */
+int root_helper(int n, int root);
+{
+	if ((root * root) == n)
+	{
+		return (root);
+	}
+
+	if ((root * root) > n)
+	{
+		return (-1);
+	}
+
+	return (root_helper(n, root + 1));
+}
