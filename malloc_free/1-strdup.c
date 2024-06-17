830670ce25398e3e6efa534da566d44f3321f212
Sam <s.olubode@alustudent.com>
2024-06-17 14:33:00 +0100

    0-create_array.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Create Array
+ * @str: arg - size of array
+ * Return: char *
+*/
+char *_strdup(char *str)
+{
+	int *p;
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(sizeof(c) * size);
+	return (p);
+}
