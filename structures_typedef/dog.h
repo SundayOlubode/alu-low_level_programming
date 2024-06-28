fbf79395531531e0a132df5c84b1c44e8ebe5ef2
Sam <s.olubode@alustudent.com>
2024-06-28 12:00:21 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+/**
+ * struct dog - struct type dog
+ * @name: dog name pointer
+ * @age: dog age in float
+ * @owner: owner pointer
+ */
+typedef struct dog
+{
 	char *name;
 	double age;
 	char *owner;
