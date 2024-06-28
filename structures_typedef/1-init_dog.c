247ac21bd9a4b223bbbb9f6352cf8034b682a264
Sam <s.olubode@alustudent.com>
2024-06-28 12:10:02 +0100

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a new dog struct
+ * @d: struct dog
+ * @name: pointer to name
+ * @age: float age
+ * @owner: pointer to owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = *name;
+	d->age = age;
+	d->owner = owner;
+}
