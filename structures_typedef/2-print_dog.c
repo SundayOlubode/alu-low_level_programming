4068a1f568f52a51196b5f64df51eefc79b64d86
Sam <s.olubode@alustudent.com>
2024-06-28 13:05:28 +0100

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * print_dog - Prints a new dog struct
+ * @d: struct dog
+*/
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == NULL)
+		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", d->name);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
