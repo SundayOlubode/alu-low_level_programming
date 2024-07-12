abbe0efd8896b4186fe87f7ab80f698ee26e11c6
Sam <s.olubode@alustudent.com>
2024-07-12 19:42:15 +0100

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void print_name(char *name, void (*f)(char *)) {
-    if (name != NULL && f != NULL) {
-        f(name);
-    }
+/**
+ * print_name - Print name with callback function
+ * @name: name to be printed
+ * @f: pointer to cb function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
 
-void print_lowercase(char *str) {
-    while (*str) {
-        printf("%c", *str);
-        str++;
-    }
-    printf("\n");
+/**
+ * print_lowercase - cb function
+ * @str: char string to print
+ */
+void print_lowercase(char *str)
+{
+	while (*str)
+	{
+		printf("%c", *str);
+		str++;
+	}
+	printf("\n");
 }
