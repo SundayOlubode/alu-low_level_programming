5ff38349833bc23ec181de2a5c25fcd38f27a516
Sam <s.olubode@alustudent.com>
2024-07-15 09:20:29 +0100

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,42 +8,48 @@
  * @filename: file name
  * @letters: chars
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    int fd;
-    ssize_t n_read, n_written;
-    char *buffer;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+    	char *buffer;
 
-    if (filename == NULL) {
-        return 0;
-    }
+	if (filename == NULL)
+	{
+		return 0;
+	}
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return 0;
+	}
 
-    buffer = (char *)malloc(letters);
-    if (buffer == NULL) {
-        close(fd);
-        return 0;
-    }
+	buffer = (char *)malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return 0;
+	}
 
-    n_read = read(fd, buffer, letters);
-    if (n_read == -1) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return 0;
+	}
 
-    n_written = write(STDOUT_FILENO, buffer, n_read);
-    if (n_written == -1 || n_written != n_read) {
-        free(buffer);
-        close(fd);
-        return 0;
-    }
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return 0;
+	}
 
-    free(buffer);
-    close(fd);
+	free(buffer);
+	close(fd);
 
-    return n_written;
+	return n_written;
 }
